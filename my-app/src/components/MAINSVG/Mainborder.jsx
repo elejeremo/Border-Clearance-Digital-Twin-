634954f6693fd25e18e12f0e7a6e9947fae9548e
ejelejeremo <leejeromefw2@gmail.com>
2024-11-13 11:52:51 +0800

    three js update camera controls

--- my-app/src/components/MAINSVG/Mainborder.jsx
+++ my-app/src/components/MAINSVG/Mainborder.jsx
@@ -1,48 +1,88 @@
-import React, { useState, useRef} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./Mainborder.css"
-import{motion} from 'framer-motion'
-import { Canvas } from '@react-three/fiber';
-import { useGLTF } from '@react-three/drei';
-import modelurl from './gltf/Gate model 1.gltf'
-import {OrbitControls} from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { DEG2RAD, degToRad } from "three/src/math/MathUtils.js";
+import gsap from 'gsap'
+import { CameraControls } from '@react-three/drei';
+import Model from "../model/model";
 
-const Model = ({ url, scale, position, rotation }) => {
-  const { scene } = useGLTF(modelurl);
-  
-
-  return (
-    <group>
-      <primitive
-        object={scene}
-        scale={scale}
-        position={position}
-        rotation={rotation}
-      />
-    </group>
-  );
-};
+const DEG45 = Math.PI / 1.5;
 
 const ThreeModel = () => {
+  
+  //animation
+
+  const cameraControlRef = useRef(); //references the camera 
+  const meshfitcamerahome = useRef();
+
+
+ 
+
   return (
     <div className="MainBorder">
-      <Canvas 
-      
-    
-      camera={{
-          position: [10, 10, 10]
-        }}>
-      <directionalLight position={[10, 10, 5]} intensity={3} />
-      <directionalLight position={[-10, -10, -5]} intensity={2} />
-        <pointLight position={[10, 10, 10]} intensity={10}/>
-        <Model
-          url=""
-          scale={0.004}
-          position={[0,0,0]}
-          rotation={[Math.PI / 7, -Math.PI / 10, 0]}
-          
+      <Canvas>
+        {/* Add PerspectiveCamera with initial position */}
+        <PerspectiveCamera
+          makeDefault
+          position={[5, 2, 5]}  // top right of the model.Adjust these values to change camera position
+          fov={75}              // Field of view
         />
-        <OrbitControls enableZoom={false} />
+
+        {/* Combine OrbitControls settings into one component */}
+
+        <CameraControls ref={cameraControlRef} 
+        max
+          autoRotate
+          enableZoom={false}     // Changed from false to match your requirements
+          maxPolarAngle={DEG2RAD * 55}
+        
+          autoRotateSpeed={0.5}
+    
+        />
+
+        {/* Lights */}
+        <directionalLight position={[10, 10, 5]} intensity={3} />
+        <directionalLight position={[-10, -10, -5]} intensity={2} />
+        <pointLight position={[10, 10, 10]} intensity={10}/>
+                 
+        {/* Model */}
+
+    
+
+        <mesh >
+          <Model
+          url=""
+          scale={0.001}
+          position={[0, 0, 0]}
+        />
+        
+        </mesh>
       </Canvas>
+
+      <div style={{ position: 'absolute', top: '0' }}>
+				<button
+					type="button"
+					onClick={() => {
+            console.log(cameraControlRef.current)
+						cameraControlRef.current.moveTo(-0.05,0,0, true)
+            cameraControlRef.current.lookInDirectionOf(0,0,0,true)
+
+  
+            //cameraControlRef.current?
+					}}
+				>
+					rotate theta 45deg
+				</button>
+				<button
+					type="button"
+					onClick={() => {
+						cameraControlRef.current?.reset(true);
+					}}
+				>
+					reset
+				</button>
+			</div>
     </div>
   );
 };
